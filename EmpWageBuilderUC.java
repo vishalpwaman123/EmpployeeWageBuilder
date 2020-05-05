2b471164b226960515d2572672b8fe0c8610b182
vishalpwaman <vishalpwaman1997@gmail.com>
2020-05-05 02:07:53 -0900

    Refactor multiple companies to manage Employee wage and Other operation

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -7,9 +7,24 @@ public class EmpWageBuilderUC{
     public static final int Part_Day=2;
     public static int Time=0;
     public static int TotalWage=0;
+    
+    public static String Company_Name;
+    public static int EmpRate;
+    public static int NumberOfDay;
+    public static int MaxHours;
+    
+    public EmpWageBuilderUC( String Company_Name , int EmpRate , int NumberOfDay , int MaxHours )
+    {
+    
+    this.Company_Name=Company_Name;
+    this.EmpRate=EmpRate;
+    this.NumberOfDay=NumberOfDay;
+    this.MaxHours=MaxHours;
+    
+    }
 
 
-    public static void CompanyWage(ArrayList<String>Company_Name,ArrayList<String>Total_Wage,String Company, int EmpRate, int NumberOfDay, int MaxHours){
+    public static int CompanyWage(){
       	
 	int Hours_Counter=0;
         int Days_Counter=0;
@@ -32,50 +47,29 @@ public class EmpWageBuilderUC{
                     break;
                 default :
                     Time=0;
-                    //System.out.println("Employee is Absent ");
-                    //System.out.println("Daily Wage is :"+Daily_Wage);
+                    
 
             }
             Hours_Counter=Hours_Counter+Time;
             Daily_Wage=EmpRate*Time;
             TotalWage=TotalWage+Daily_Wage;
-           // System.out.println("Day "+(Days_Counter)+" Wage is :"+Daily_Wage);
+           
         }
 
         Hours_Counter=Hours_Counter-Time;
         TotalWage=TotalWage-Daily_Wage;
-        Company_Name.add(Company);  
-        Total_Wage.add(Integer.toString(TotalWage));
-        //System.out.println("Total Hours :"+Hours_Counter);
-        //System.out.println("Total Days :"+Days_Counter);
-        //System.out.println("Total "+Company+" Employee Wage :"+TotalWage);
-	//EWageStorage.put(Company,TotalWage);
+        return TotalWage;
+	
     }
 
-	public static void Display(ArrayList<String>Company_Name,ArrayList<String>Total_Wage)
-	{
-	//Enumeration i = EWageStorage.elements();
-	//Enumeration j = EWageStorage.keys();
-	Iterator C_Name=Company_Name.iterator();
-	Iterator T_Wage=Total_Wage.iterator();
-	
-	for (; C_Name.hasNext() && T_Wage.hasNext() ;)
-	{
-	
-	System.out.println("Company Name :"+C_Name.next()+ "         Total Employee Wage :"+T_Wage.next());
-	
-	}
-	
-	}
+
     public static void main(String[] args){
 
-        //int CheckStatus = (int)(Math.floor(Math.random()*10)%3);
-	//Dictionary EWageStorage = new Hashtable();
-	ArrayList<String>Company_Name=new ArrayList<String>();
-	ArrayList<String>Total_Wage= new ArrayList<String>();
-        CompanyWage(Company_Name,Total_Wage,"Dmart",20,2,10);
-	CompanyWage(Company_Name,Total_Wage,"RelianceMart",40,4,20);
-	Display(Company_Name,Total_Wage);
+        EmpWageBuilderUC DmartObject = new EmpWageBuilderUC("Dmart",20,2,10);	
+        System.out.println("Total "+DmartObject.Company_Name+" Employee Wage :"+DmartObject.CompanyWage());
+	EmpWageBuilderUC RelianceObject = new EmpWageBuilderUC("RelianceMart",40,4,20);
+	System.out.println("Total "+RelianceObject.Company_Name+" Employee Wage :"+RelianceObject.CompanyWage());
+	
     }
 
     }
