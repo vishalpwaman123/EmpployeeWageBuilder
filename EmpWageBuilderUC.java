8d38cae2bb1bc55698e5fad4274469f062edb4d5
vishalpwaman <vishalpwaman1997@gmail.com>
2020-05-05 01:41:44 -0900

    Added. 'useCase3' Add Part Time Employee And Wage

--- EmpWageBuilderUC.java
+++ EmpWageBuilderUC.java
@@ -1,23 +1,33 @@
-public class EmpWageBuilderUC2{
-	public static int Wage_Per_Hour=20;
-	public static int Full_Day=8;	
+public class EmpWageBuilderUC{
+	public static int Wage_Per_Hours=20;
+	public static int Full_Time=16;
+	public static int Part_Time=8;
+	public static int Time=0;
 
 	public static void PresentAbsent(int Status){
 		int Daily_Wage=0;
-		if ( Status == 1)
+		if ( Status == 1 )
 		{
-			Daily_Wage=Wage_Per_Hour*Full_Day;
-			System.out.println("Daily Wage is :"+Daily_Wage); 
+			Time=Full_Time;
+			 
+		}else if ( Status == 2 )
+		{
+			Time=Part_Time;
 		}else
 		{
 			System.out.println("Employee is Absent ");
 			System.out.println("Daily Wage is :"+Daily_Wage);
+			return ;
 		}
+		Daily_Wage=Wage_Per_Hours*Time;
+		System.out.println("Daily Wage is :"+Daily_Wage);
+		return;
+		
 	}
 	
 	public static void main(String[] args){
 
-	int CheckStatus = (int)(Math.floor(Math.random()*10)%2);
+	int CheckStatus = (int)(Math.floor(Math.random()*10)%3);
 	
 	PresentAbsent(CheckStatus);
 
